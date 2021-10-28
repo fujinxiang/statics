5051a01f7172ad251b130084307ea08f39eac773
付金祥 <4088233+fujinxiang@users.noreply.github.com>
2021-10-28 09:08:04 +0800

    add file

---
+++ e7374d95-d999-4c6d-8acd-1bd9710835af-ab633e80b791b70eb80808996aa28f64.js
@@ -0,0 +1,4 @@
+// 纵越南北的民族文化融合之线上研学旅行临时跳转
+if(window.location.href.indexOf('667435488328527872')>-1){window.location.href='https://ysgd.ke.seewo.com/live/interactivity/667544865748410368'}
+
+if(window.location.href.indexOf('679699062246539264')>-1){window.location.href='https://gzby.ke.seewo.com/live/interactivity/680430838396293120'}
